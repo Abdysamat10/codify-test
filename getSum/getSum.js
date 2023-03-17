a400c4b7824c18bd9da15b5b641a667144f5cfff
samat <samat020404@gmail.com>
2023-03-17 23:18:13 +0800

    change getSum.js

--- getSum/getSum.js
+++ getSum/getSum.js
@@ -5,5 +5,5 @@ function getSum(prices, discount) {
     return discountedTotal;
 }
 console.log(getSum([100, 200, 300], 30));
-// console.log(getSum([150, 250, 150], 10));
-// console.log(getSum([1000], 50));
+console.log(getSum([150, 250, 150], 10));
+console.log(getSum([1000], 50));
