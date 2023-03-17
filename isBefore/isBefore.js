9ae5e6e3b6ca6b257d4a253c142e279a4b4cb3c6
samat <samat020404@gmail.com>
2023-03-17 22:05:48 +0800

    chenge isBefore

--- isBefore/isBefore.js
+++ isBefore/isBefore.js
@@ -1,5 +1,7 @@
 function isBefore(target, base) {
-  return target > base || false;
+  const target2 = new Date(target);
+  const base2 = new Date(base);
+  return target2 < base2 || false;
 }
 console.log(isBefore('01.01.2023', '01.02.2022'));
 console.log(isBefore('01.02.2022', '01.01.2023'));
