c11a3fecf07f90c11d0170f0f368a78259c0fa65
samat <samat020404@gmail.com>
2023-03-17 22:24:49 +0800

    false

--- isBefore/isBefore.js
+++ isBefore/isBefore.js
@@ -1,7 +1,7 @@
 function isBefore(target, base) {
   const target2 = new Date(target);
   const base2 = new Date(base);
-  return target2 < base2 || false;
+  return target2 < base2
 }
 console.log(isBefore('01.01.2023', '01.02.2022'));
 console.log(isBefore('01.02.2022', '01.01.2023'));
