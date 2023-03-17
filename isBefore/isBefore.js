c62fc5d8274d5f3eddf2e9248bc2ac27f3a44c79
Abdysamat10 <64656988+Abdysamat10@users.noreply.github.com>
2023-03-17 22:33:05 +0800

    Delete isBefore directory

--- isBefore/isBefore.js
+++
@@ -1,6 +0,0 @@
-function isBefore(target, base) {
-  return target > base || false;
-}
-console.log(isBefore('01.01.2023', '01.02.2022'));
-console.log(isBefore('01.02.2022', '01.01.2023'));
-console.log(isBefore('01.02.2022', '01.02.2022'));
